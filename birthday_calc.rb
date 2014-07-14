c86f0a67995760ec089ee4b906b5868ec34dda90
Katie Hoffman <ktahoffman@gmail.com>
2014-07-14 16:32:50 -0400

    fixes up tests so that they come in an order that makes more sense

--- birthday_calc.rb
+++ birthday_calc.rb
@@ -3,74 +3,21 @@ require 'time'
 require 'date'
 
 class BirthdayCalc
-  attr_reader :birthday, :today, :this_years_birthday
 
   def initialize(birthday)
-    @today = Date.today
-    reformatted_birthday = birthday[3..4] + "/" + birthday[0..1]
-
-    @birthday = Date.parse(reformatted_birthday + "/" + birthday[6..-1])
-    @this_years_birthday = Date.parse(reformatted_birthday + "/" + today.year.to_s)
-  end
-
-  def born_yet?
-    if birthday > today
-      raise FutureError
-    else 
-      true
-    end
-  end
-
-  def calculate_age
-    (today - birthday).to_i / 365
+    # code here
   end
 
   def age
-    if born_yet?
-      if birthday.month < today.month
-        calculate_age
-      elsif birthday.month > today.month
-        calculate_age - 1
-      elsif birthday.month == today.month
-        if birthday.day <= today.day
-          calculate_age
-        else  # birthday.day > today.day
-          calculate_age - 1
-        end
-      end
-    end
-  end
-
-  def calculate_days
-    (this_years_birthday - today).to_i.abs
+    # code here
   end
 
   def number_of_days
-    if born_yet?
-      if this_years_birthday.month > today.month
-        calculate_days
-      elsif this_years_birthday.month < today.month
-        365 - calculate_days
-      else
-        if this_years_birthday.day >= today.day
-          calculate_days
-        else
-          365 - calculate_days
-        end
-      end
-    end
+    # code here
   end
 
   def birthday?
-    if number_of_days == 0
-      true
-    else
-      false
-    end
+    # code here
   end
-
-end
-
-class FutureError < StandardError
 
 end
--- spec/birthday_calc_spec.rb
+++ spec/birthday_calc_spec.rb
@@ -12,26 +12,26 @@ describe BirthdayCalc do
   let(:yesterday)           {BirthdayCalc.new("07/13/2000")}
 
   describe '#age' do
-    it 'gives age of someone born in the past' do
+    it 'gives age of someone born in the past whose birthday is in the current month but has not yet come to pass' do
       expect(past.age).to eq(24)
     end
-    it 'gives age of someone born in the past' do
-      expect(katie.age).to eq(25)
-    end
-    it 'gives age of someone born in the current year' do
+    it 'gives age of someone born in the current year, current month, and current day' do
       expect(present.age).to eq(0)
     end
-    it 'gives age of someone born in the current year and current month and current day' do
+    it 'gives age of someone born fourteeen years ago from yesterday' do
+      expect(yesterday.age).to eq(14)
+    end
+    it 'gives age of someone born in the past whose birthday is in a month that passed already' do
+      expect(katie.age).to eq(25)
+    end
+    it 'gives age of someone whose birthday is the current month and current day' do
       expect(bd_today.age).to eq(14)
     end
-    it 'gives age of someone born in the current month' do
+    it 'gives age of someone whose birthday is tomorrow' do
       expect(tomorrow.age).to eq(13)
     end
-    it 'gives age of someone born in the current month' do
+    it 'gives age of someone with a birthday the day after tomorrow' do
       expect(day_after_tomorrow.age).to eq(13)
-    end
-    it 'gives age of someone born in the current month' do
-      expect(yesterday.age).to eq(14)
     end
     it 'raises error when birthday is in the future' do
       expect{ future.age }.to raise_error(FutureError)
@@ -39,23 +39,23 @@ describe BirthdayCalc do
   end
 
   describe '#number_of_days' do
-    it 'gives number of days until a given date' do
-      expect(past.number_of_days).to eq(6)
-    end
-    it 'gives number of days until a given date next year' do
-      expect(katie.number_of_days).to eq(339)
-    end
-    it 'gives number of days until a given date' do
-      expect(present.number_of_days).to eq(0)
-    end
-    it 'gives age of someone born in the current year and current month and current day' do
-      expect(bd_today.number_of_days).to eq(0)
-    end
-    it 'gives age of someone born in the current month' do
+    it "gives a number one when someone's birthday is tomorrow" do
       expect(tomorrow.number_of_days).to eq(1)
     end
-    it 'gives age of someone born in the current month' do
+    it 'gives number of days until an upcoming birthday' do
+      expect(past.number_of_days).to eq(6)
+    end
+    it 'gives number of days until a birthday for that month and day' do
+      expect(present.number_of_days).to eq(0)
+    end
+    it 'gives number of days until a birthday for that month and day' do
+      expect(bd_today.number_of_days).to eq(0)
+    end
+    it 'gives number of days until an upcoming birthday, accounting for the fact that it may be the next calendar year' do
       expect(yesterday.number_of_days).to eq(364)
+    end
+    it 'gives number of days until an upcoming birthday, accounting for the fact that it may be the next calendar year' do
+      expect(katie.number_of_days).to eq(339)
     end
     it 'raises error when birthday is in the future' do
       expect{ future.number_of_days }.to raise_error
@@ -63,13 +63,7 @@ describe BirthdayCalc do
   end
 
   describe '#birthday?' do
-    it 'returns false if it is not the month and day of the birthday' do
-      expect(past.birthday?).to eq(false)
-    end
-    it 'returns false if it is not the month and day of the birthday'  do
-      expect(katie.birthday?).to eq(false)
-    end
-    it 'returns true if it is the month and day of the birthday' do
+    it 'returns true if it is the month and day and year of birthday' do
       expect(present.birthday?).to eq(true)
     end
     it 'returns true if it is the month and day of the birthday' do
@@ -77,6 +71,12 @@ describe BirthdayCalc do
     end
     it 'raises error when birthday is in the future' do
       expect{ future.birthday? }.to raise_error
+    end
+    it 'returns false if it is not the month and day of the birthday' do
+      expect(past.birthday?).to eq(false)
+    end
+    it 'returns false if it is not the month and day of the birthday'  do
+      expect(katie.birthday?).to eq(false)
     end
     it 'returns false if it is not the month and day of the birthday' do
       expect(tomorrow.birthday?).to eq(false)
